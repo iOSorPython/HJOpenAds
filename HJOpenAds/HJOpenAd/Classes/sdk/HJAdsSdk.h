00fadcd4075b037e5f651cd186123a3154e5e52d
forpenta <forpenta@163.com>
2025-09-16 17:09:21 +0800

    2222

--- HJOpenAds/HJOpenAd/Classes/sdk/HJAdsSdk.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface HJAdsSdk : NSObject
-+(instancetype) sharedAds;
--(void)setupSDKWithAppId:(NSString *)appId;
--(void)setDebugEnable:(BOOL)enable;
--(void) startAdPreviously:(NSString *)rewardId interstitialId:(NSString *)interstitialId fullScreenId:(NSString *)fullScreenId userId:(NSString *)userId;
-@end
